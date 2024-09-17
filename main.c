40940cad5b052dda1c01a821781b682783599dbc
Heitor <heitor_portugal1@hotmail.com>
2024-09-16 22:07:24 -0300

    descobri um problema nos nomes dos jogadores. Todos os jogadores da lista ficam com o nome do ultimo jogador cadastrado

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "pilha.h"
 #include "jogador.h"
 
@@ -33,38 +34,61 @@ int main(void)
         scanf("%s %i", nome, &numero);
         Jogadores[aaa].nome = nome;
         Jogadores[aaa].num = numero;
+        printf("Cadastrado o jogador: Nome: %s Numero: %i", Jogadores[aaa].nome, Jogadores[aaa].num);
         scanf("");
     }
     PILHA *pilha = selection_sort(Jogadores, n);
     while(pilha_tamanho(pilha) > 0)
     {
       ITEM *aux = pilha_desempilhar(pilha);
-      printf("Num: %i\n", aux->num);
+      printf("Nome: %s\nNum: %i\n", aux->nome, aux->num);
     }
     pilha_apagar(&pilha);
 }
 
 PILHA *selection_sort(Jogador *Jogadores, int n)
 {
-  int i, j, min, aux;
+  printf("\n\n----------- JOGADORES ------------\n\n");
+  for(int i = 0; i < n; i++)
+  {
+    printf("%i: Nome: %s - Numero: %i\n", i, Jogadores[i].nome, Jogadores[i].num);
+  }
+  printf("\n ----------------------------------\n");
+  int i, j, min, auxnu;
+  char *auxno;
   for (i = 0; i < (n-1); i++) 
   {
-     min = i;
-     for (j = (i+1); j < n; j++) {
-       if(Jogadores[j].num < Jogadores[min].num) 
-         min = j;
-     }
-     if (i != min) {
-       aux = Jogadores[i].num;
-       Jogadores[i].num = Jogadores[min].num;
-       Jogadores[min].num = aux;
-     }
-  }
-  PILHA *pilha = pilha_criar();
-  for(int i = 0; i < n; i ++)
-  {
+    min = i;
+    for (j = (i+1); j < n; j++) {
+      if(Jogadores[j].num <= Jogadores[min].num) 
+      {
+        if (Jogadores[j].num == Jogadores[min].num)
+        {
+          printf("Vou comparar os dois nomes: \nNome J: %s\nNome min: %s", Jogadores[j].nome, Jogadores[min].nome);
+          if (strcmp(Jogadores[j].nome, Jogadores[min].nome) == -1)
+          {
+              min = j;
+          }
+        }
+        else {
+          min = j;
+        }
+      }
+    }
+    if (i != min) {
+      auxnu = Jogadores[i].num;
+      strcpy(auxno, Jogadores[i].nome);
+      Jogadores[i].num = Jogadores[min].num;
+      strcpy(Jogadores[i].nome, Jogadores[min].nome);
+      Jogadores[min].num = auxnu;
+      strcpy(Jogadores[min].nome, auxno);
+    }
+    }
+    PILHA *pilha = pilha_criar();
+    for(int i = 0; i < n; i ++)
+    {
     ITEM *aux = jogador_criar(Jogadores[i].nome, Jogadores[i].num);
     pilha_empilhar(aux, pilha);
-  }
-  return pilha;
+    }
+    return pilha;
 }
